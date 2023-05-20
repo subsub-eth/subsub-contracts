711ae1d830601dc2bad9ea8d6dcf1e9bef1617dd
Patrick Sy <patrick.sy@get-it.us>
2023-05-20 21:24:00 +0200

    upgradeable ownerwhip

---
+++ src/OwnableByERC721Upgradeable.sol
@@ -0,0 +1,147 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {ContextUpgradeable} from "openzeppelin-contracts-upgradeable/contracts/utils/ContextUpgradeable.sol";
+import {Initializable} from "openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
+
+import {IERC721} from "openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+
+/**
+ * @dev copied from OZ upgradeable contracts, changed to use an ERC721 for
+ * ownership.
+ *
+ * Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+// TODO fix comments
+// TODO check contract to an ERC721 contract
+abstract contract OwnableByERC721Upgradeable is
+    Initializable,
+    ContextUpgradeable
+{
+    address private _ownerContract;
+    uint256 private _ownerTokenId;
+
+    event OwnershipTransferred(
+        address indexed previousOwnerContract,
+        uint256 indexed previousOwnerTokenId,
+        address indexed newOwnerContract,
+        uint256 newOwnerTokenId
+    );
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    function __OwnableByERC721_init(address ownerContract, uint256 ownerTokenId)
+        internal
+        onlyInitializing
+    {
+        __OwnableByERC721_init_unchained(ownerContract, ownerTokenId);
+    }
+
+    function __OwnableByERC721_init_unchained(
+        address ownerContract,
+        uint256 ownerTokenId
+    ) internal onlyInitializing {
+        _transferOwnership(ownerContract, ownerTokenId);
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        _checkOwner();
+        _;
+    }
+
+    /**
+     * @dev Returns the ERC721 contract address and the tokenId
+     */
+    function owner() public view virtual returns (address, uint256) {
+        return (_ownerContract, _ownerTokenId);
+    }
+
+    /**
+     * @dev Returns the owner of the token in the ERC721 contract, address(0)
+     * if the contract is set to address(0)
+     */
+    function ownerAddress() public view virtual returns (address) {
+        if (_ownerContract == address(0)) {
+            return address(0);
+        }
+        return IERC721(_ownerContract).ownerOf(_ownerTokenId);
+    }
+
+    /**
+     * @dev Throws if the sender is not the owner.
+     */
+    function _checkOwner() internal view virtual {
+        require(
+            IERC721(_ownerContract).ownerOf(_ownerTokenId) == _msgSender(),
+            "Ownable: caller is not the owner"
+        );
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby disabling any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0), 0);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(
+        address newOwnerContract,
+        uint256 newOwnerTokenId
+    ) public virtual onlyOwner {
+        // TODO check supportsInterface
+        require(
+            newOwnerContract != address(0),
+            "Ownable: new owner contract is the zero address"
+        );
+        _transferOwnership(newOwnerContract, newOwnerTokenId);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(
+        address newOwnerContract,
+        uint256 newOwnerTokenId
+    ) internal virtual {
+        address oldOwnerContract = _ownerContract;
+        uint256 oldOwnerTokenId = _ownerTokenId;
+        _ownerContract = newOwnerContract;
+        _ownerTokenId = newOwnerTokenId;
+        emit OwnershipTransferred(
+            oldOwnerContract,
+            oldOwnerTokenId,
+            newOwnerContract,
+            newOwnerTokenId
+        );
+    }
+
+    /**
+     * @dev This empty reserved space is put in place to allow future versions to add new
+     * variables without shifting down storage in the inheritance chain.
+     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+     */
+    uint256[48] private __gap;
+}
---
+++ test/OwnableByERC721Upgradeable.t.sol
@@ -0,0 +1,150 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "../src/OwnableByERC721Upgradeable.sol";
+
+import {ERC721Mock} from "openzeppelin-contracts/contracts/mocks/ERC721Mock.sol";
+
+contract MyOwnable is OwnableByERC721Upgradeable {
+
+  // simple default implementation
+  function init(address kontract, uint256 tokenId) initializer public {
+    __OwnableByERC721_init(kontract, tokenId);
+  }
+}
+
+contract OwnableByERC721UpgradeableTest is Test {
+    event OwnershipTransferred(
+        address indexed previousOwnerContract,
+        uint256 indexed previousOwnerTokenId,
+        address indexed newOwnerContract,
+        uint256 newOwnerTokenId
+    );
+
+    ERC721Mock private nft;
+
+
+    MyOwnable private myOwnable;
+
+    function setUp() public {
+      nft = new ERC721Mock("test", "test");
+    }
+
+
+    function testInitOwnership(uint256 tokenId) public {
+      nft.mint(address(this), tokenId);
+
+      MyOwnable mo = new MyOwnable();
+
+      (address _contract, uint256 _id) = mo.owner();
+      assertEq(_contract, address(0), "contract is set to 0");
+      assertEq(_id, 0, "id not set");
+      assertEq(address(0), mo.ownerAddress(), "no owner address, yet");
+
+      vm.expectEmit(true, true, true, true);
+      emit OwnershipTransferred(
+            address(0),
+            0,
+            address(nft),
+            tokenId
+        );
+
+      mo.init(address(nft), tokenId);
+
+      (_contract, _id) = mo.owner();
+      assertEq(address(nft), _contract, "owner contract set");
+      assertEq(tokenId, _id, "id set");
+      assertEq(address(this), mo.ownerAddress(), "owner address is this");
+    }
+
+
+    function testTransfer(uint256 tokenId) public {
+      uint256 initId = 10;
+      vm.assume(tokenId != initId);
+
+      nft.mint(address(this), initId);
+      nft.mint(address(this), tokenId);
+
+      MyOwnable mo = new MyOwnable();
+      mo.init(address(nft), initId);
+
+      vm.expectEmit(true, true, true, true);
+      emit OwnershipTransferred(
+            address(nft),
+            initId,
+            address(nft),
+            tokenId
+        );
+      mo.transferOwnership(address(nft), tokenId);
+
+      (address _contract, uint256 _id) = mo.owner();
+      assertEq(_contract, address(nft), "contract is set to nft");
+      assertEq(_id, tokenId, "id set to tokenId");
+      assertEq(address(this), mo.ownerAddress(), "owner address set");
+    }
+
+    function testTransfer_notOwner(uint256 tokenId, address otherUser) public {
+      uint256 initId = 10;
+      vm.assume(tokenId != initId && otherUser != address(this));
+
+      nft.mint(address(this), initId);
+      nft.mint(address(this), tokenId);
+
+      MyOwnable mo = new MyOwnable();
+      mo.init(address(nft), initId);
+
+      vm.expectRevert("Ownable: caller is not the owner");
+      vm.prank(otherUser);
+      mo.transferOwnership(address(nft), tokenId);
+    }
+
+    function testTransfer_notZeroArress() public {
+      uint256 initId = 10;
+
+      nft.mint(address(this), initId);
+
+      MyOwnable mo = new MyOwnable();
+      mo.init(address(nft), initId);
+
+      vm.expectRevert("Ownable: new owner contract is the zero address");
+      mo.transferOwnership(address(0), 1);
+    }
+
+    function testRenounceOwnership() public {
+      uint256 initId = 10;
+
+      nft.mint(address(this), initId);
+
+      MyOwnable mo = new MyOwnable();
+      mo.init(address(nft), initId);
+
+      vm.expectEmit(true, true, true, true);
+      emit OwnershipTransferred(
+            address(nft),
+            initId,
+            address(0),
+            0
+        );
+      mo.renounceOwnership();
+
+      (address _contract, uint256 _id) = mo.owner();
+      assertEq(_contract, address(0), "contract is set to 0");
+      assertEq(_id, 0, "id set to 0");
+      assertEq(address(0), mo.ownerAddress(), "owner address is 0");
+    }
+
+    function testRenounceOwnership_onlyOwner() public {
+      uint256 initId = 10;
+      address otherUser = address(10023);
+
+      nft.mint(address(this), initId);
+
+      MyOwnable mo = new MyOwnable();
+      mo.init(address(nft), initId);
+
+      vm.expectRevert("Ownable: caller is not the owner");
+      vm.prank(otherUser);
+      mo.renounceOwnership();
+    }
+}
