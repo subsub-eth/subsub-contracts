39abeb04444e8869253477ef58a82953950f8d33
suiiii <suiiii@suiiii.rip>
2024-07-01 12:55:22 +0200

    feat(maxSupply): add simple test

---
+++ test/subscription/MaxSupply.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import {Math} from "openzeppelin-contracts/contracts/utils/math/Math.sol";
+import "../../src/subscription/MaxSupply.sol";
+
+contract TestMaxSupply is MaxSupply {
+    constructor(uint256 _supply) initializer {
+        __MaxSupply_init(_supply);
+    }
+
+    function maxSupply() public view returns (uint256) {
+        return _maxSupply();
+    }
+}
+
+contract MaxSupplyTest is Test {
+    using Math for uint256;
+
+    TestMaxSupply private ms;
+
+    function setUp() public {
+    }
+
+    function testSetMaxSupply(uint256 _supply) public {
+        ms = new TestMaxSupply(_supply);
+        assertEq(_supply, ms.maxSupply(), "supply set");
+    }
+}
